5eb63a2816fbe1cc28e8817cd343639cc6d2971f
Celiasch <clea.eliasch@htl-salzburg.ac.at>
2019-02-24 19:44:57 +0100

    Added Script to Buttons

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float minPosLeft;
+    public float maxPosRight;
+    public float padding;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+        minPosLeft = -2.60f;
+        maxPosRight = 2.60f;
+        padding = 0.2f;
+
+        SetupMoveBoundaries();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        //Methode Move() move character left right with arrow keys)
+
+    }
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var newPosX = Mathf.Clamp(transform.position.x + deltaX, minPosLeft, maxPosRight);
+
+        transform.position = new Vector2(newPosX, transform.position.y);
+    }
+
+    private void SetupMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        minPosLeft = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x + padding;
+        maxPosRight = gameCamera.ViewportToWorldPoint(new Vector3(1f, 0f, 0f)).x - padding;
+
+    }
 }
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour {
+public class SceneLoader : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void GoToWinOverScene()
+    {
+        SceneManager.LoadScene("WinScene");
+    }
+    public void GoToGameOverScene()
+    {
+        SceneManager.LoadScene("GameOverScene");
+    }
+
 }
