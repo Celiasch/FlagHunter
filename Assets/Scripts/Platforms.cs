d85e3d7a30111c44d19397f7e67768c638f8d6ac
Celiasch <clea.eliasch@htl-salzburg.ac.at>
2019-02-26 10:29:32 +0100

    moving Platforms upwards

--- Assets/Scripts/Platforms.cs
+++ Assets/Scripts/Platforms.cs
@@ -1,16 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Platforms : MonoBehaviour {
+public class Platforms : MonoBehaviour
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+{
+
+    void Update()
+
+    {
+        transform.Translate(0, Time.deltaTime * 1, 0, Space.World);
+
+    }
+
 }
