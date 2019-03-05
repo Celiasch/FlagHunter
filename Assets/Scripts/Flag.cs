a019f4719a30ac66e8a063e24b25bed1c11ff29a
Celiasch <clea.eliasch@htl-salzburg.ac.at>
2019-03-05 08:35:34 +0100

    made Random color Flag work

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Flag : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+public class Flag : MonoBehaviour
+{
+    public GameObject circleColor;
+
+
+    private System.Random rnd = new System.Random();
+
+
+    void Start ()
+    {
+        SpriteRenderer circleRenderer = circleColor.GetComponent<SpriteRenderer>(); // renamed the SpriteRenderer from circRenderer (not sure if intetional)
+        byte r = (byte)rnd.Next(0, 256);
+        byte g = (byte)rnd.Next(0, 256);
+        byte b = (byte)rnd.Next(0, 256);
+        byte a = (byte)rnd.Next(0, 256); // dont if the circle in the apha channel is intentional or by mistake
+
+        circleRenderer.color = new Color32(r, g, b, a);
+    }
+
 }
